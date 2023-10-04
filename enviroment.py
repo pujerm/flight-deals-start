5c2ae5168ba762bdf954d8d4bfcf3bcf79446f60
Mateusz <101259934+pujerm@users.noreply.github.com>
2023-10-04 12:23:39 +0200

    Update enviroment.py

--- enviroment.py
+++ enviroment.py
@@ -1,4 +1,4 @@
-account_sid = "AC76203074a459952d1245a88b821cddd4"
-auth_token = "8fcdeb561cf2d3ef5b14737e519de453"
-apikey = "1I0MFBjZDOBQyaAV1ShDKQeBhEgwFbT-"
+account_sid = "account_sid"
+auth_token = "auth_token"
+apikey = "apikey"
 
